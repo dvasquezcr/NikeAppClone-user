dc4aecc33a8120658268792b18832272f2936904
David <davidvasquez07@gmail.com>
2023-07-08 19:13:43 -0600

    Se crea ProductDetatilsScreen

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import ProductsScreeen from './src/screens/productsScreeen';
+import ProductDetailsScreen from './src/screens/ProductDetailsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <ProductsScreeen />
+      {/* <ProductsScreeen /> */}
+      <ProductDetailsScreen />
 
       <StatusBar style="auto" />
     </View>
